32613d44a41ee2a5a1f6ee946c99a7385dcd246b
IlyaRid <redenkoid@gmail.com>
2022-10-14 00:03:43 +0300

    Working with API

--- api.js
+++ api.js
@@ -1,12 +1,22 @@
+let index = 2;
+
 const getData = async (url) => {
-  const obj = fetch(url);
-  return (await obj).json();
+  try {
+    const response = await fetch(url);
+    const obj = await response.json();
+    console.log(obj);
+    console.log(`title${[index]}`);
+    document.getElementById(`title${[index]}`).innerHTML = obj.name;
+    document.getElementById(`img${[index]}`).src = obj.image.original;
+    document.getElementById(`description${[index]}`).innerHTML = obj.summary;
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
-const data = "https://api.tvmaze.com/search/shows?q=girls";
-try {
-  const res = await getData(data);
-  document.querySelector(".card-title").innerHTML = res[0].show.name;
-} catch (error) {
-  console.error(error.message);
-}
+do {
+  const data = `https://api.tvmaze.com/shows/${[index]}`;
+  console.log(index);
+  getData(data);
+  index++;
+} while (index < 3);
