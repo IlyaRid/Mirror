b5ce9b65681239429d39c625a51cd3c1de3ca779
IlyaRid <redenkoid@gmail.com>
2022-10-14 12:30:02 +0300

    Working with API

--- api.js
+++ api.js
@@ -1,22 +1,34 @@
-let index = 2;
-
 const getData = async (url) => {
   try {
     const response = await fetch(url);
     const obj = await response.json();
     console.log(obj);
-    console.log(`title${[index]}`);
-    document.getElementById(`title${[index]}`).innerHTML = obj.name;
-    document.getElementById(`img${[index]}`).src = obj.image.original;
-    document.getElementById(`description${[index]}`).innerHTML = obj.summary;
+    showCard(obj);
   } catch (error) {
     console.log(error.message);
   }
 };
 
-do {
-  const data = `https://api.tvmaze.com/shows/${[index]}`;
-  console.log(index);
+for (let index = 1; index <= 3; index++) {
+  const data = `https://api.tvmaze.com/shows/${index}`;
   getData(data);
-  index++;
-} while (index < 3);
+}
+
+function showCard(serial) {
+  const cards = document.querySelector(".row");
+
+  const card = document.createElement("div");
+  card.classList.add("col");
+  card.innerHTML = `<div class="card h-100">
+  <img src="${serial.image.original}" class="card-img-top" alt="${serial.name}" />
+  <div class="card-body">
+    <h5 class="card-title">${serial.name}</h5>
+    <p class="card-text">
+      ${serial.summary}
+    </p>
+  </div>
+  <div class="card-footer">
+    <small class="text-muted">Ended: ${serial.ended}</small>
+  </div></div>`;
+  cards.appendChild(card);
+}
