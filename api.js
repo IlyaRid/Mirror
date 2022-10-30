9aa37f42d1563ec52884f6fb7dda422a4212d7ca
IlyaRid <redenkoid@gmail.com>
2022-10-30 23:54:35 +0300

    Working with modal window

--- api.js
+++ api.js
@@ -1,5 +1,4 @@
 const searchApi = `https://api.tvmaze.com/search/shows?q=`;
-let card;
 
 const getData = async (url) => {
   try {
@@ -14,16 +13,12 @@ const getData = async (url) => {
       });
     } else {
       showCard(api);
+      // console.log(card);
     }
   } catch (error) {
     console.log(error.message);
   }
 };
-
-for (let index = 1; index <= 8; index++) {
-  const data = `https://api.tvmaze.com/shows/${index}`;
-  getData(data);
-}
 
 function showCard(serial) {
   const cards = document.querySelector(".row");
@@ -43,10 +38,27 @@ function showCard(serial) {
   </div>`;
 
   cards.appendChild(col);
-
-  card = document.querySelector(".card");
-  console.log(card);
 }
+
+const paint = async () => {
+  async function paintStartCards() {
+    for (let index = 1; index <= 8; index++) {
+      const data = `https://api.tvmaze.com/shows/${index}`;
+      await getData(data);
+    }
+  }
+  await paintStartCards();
+  const card = document.querySelectorAll(".card");
+  // console.log(card);
+  for (let i = 0; i < card.length; i++) {
+    card[i].addEventListener("click", () => {
+      openModule(`https://api.tvmaze.com/shows/${card[i].id}`);
+      // console.log(card[i].id);
+    });
+  }
+};
+
+paint();
 
 const form = document.querySelector("form");
 //console.log(form);
@@ -70,21 +82,28 @@ const openModule = async (url) => {
     const windowApi = await response.json();
     console.log(windowApi);
 
-    const window = document.querySelector(".moduleWin");
-    window.innerHTML = `<img src="" class="card-img-top" alt="" />
-     <div class="truncate-text">
-      <h5 class="card-title">Name</h5>
-      <p class="card-text">
-        Description
-      </p>
-     </div>
-      <p class="card-text" style="margin-top: 15px;"><small>Ended: </small></p>`;
+    const window = document.querySelector(".moduleBack");
+    window.classList.add("module-show");
+    window.innerHTML = `
+    <div class="moduleWin">
+      <img src="${windowApi.image.original}" class="img-module" alt="${windowApi.name}" />
+      <div class="module-text">
+        <h5 class="title-module">${windowApi.name}</h5><br>
+        <p><b>Genres:</b> ${windowApi.genres}<br><br>
+        <b>Rating: <span class="rating">${windowApi.rating.average}</span></b>
+        ${windowApi.summary}</p>
+        <p class="module-ended"><small>Ended: ${windowApi.ended}</small></p>
+      </div>
+    </div>`;
+    const rating = document.querySelector(".rating");
+    if (windowApi.rating.average >= 8) {
+      rating.classList.add("rating-green");
+    } else if (windowApi.rating.average < 8 && windowApi.rating.average >= 6) {
+      rating.classList.add("rating-orange");
+    } else {
+      rating.classList.add("rating-red");
+    }
   } catch (error) {
     console.log(error.message);
   }
 };
-
-card.addEventListener("click", () => {
-  openModule(`https://api.tvmaze.com/shows/${card.id}`);
-  console.log(moduleWin.id);
-});
